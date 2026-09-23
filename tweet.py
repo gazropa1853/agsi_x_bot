909b5e686afb37823311ada503cf6639280bfd36
gazropa1853 <gaz.ropa.1853@gmail.com>
2026-09-23 14:50:30 +0200

    Create tweet.py

---
+++ tweet.py
@@ -0,0 +1,16 @@
+import os
+import requests
+ 
+API_KEY = os.environ["AGSI_API_KEY"]
+ 
+headers = {
+"x-key": API_KEY
+}
+ 
+response = requests.get(
+"https://agsi.gie.eu/api",
+headers=headers
+)
+ 
+print(response.status_code)
+print(response.text[:500])
